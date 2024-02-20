00e362e4d252b3e6992f5557e9c9f611379314fd
Luis C. García <lugarcia@lacolonial.com.do>
2024-02-20 08:34:32 -0400

    updated folder structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(
     name='dgii_rnc',
-    version='0.3',
+    version='0.4',
     author='Luis C Garcia',
     packages=find_packages(where="src"),
     install_requires=[
